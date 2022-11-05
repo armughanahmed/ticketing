0ab86d39c88fc4af5e4e05902f703f63c58f8328
armughanahmed <50084543+armughanahmed@users.noreply.github.com>
2022-11-05 15:28:45 +0500

    mongoose setup
    mongoose initial setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import "express-async-errors";
+import mongoose from "mongoose";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -22,6 +23,17 @@ app.all("*", async (req, res) => {
 
 app.use(errorHandler);
 
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Connected To MongoDB");
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 app.listen(3000, () => {
   console.log("listening on port 3000!!");
 });
+
+start();
