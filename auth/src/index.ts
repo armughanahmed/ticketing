73b5582d585297da1c66db666dd29559e0a332e7
armughanahmed <50084543+armughanahmed@users.noreply.github.com>
2022-11-05 17:56:25 +0500

    cookie added
    - cookie session added.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import "express-async-errors";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -11,7 +12,16 @@ import { errorHandler } from "./middlewares/error-handler";
 
 const app = express();
 
+app.set("trust-proxy", true);
+
 app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
