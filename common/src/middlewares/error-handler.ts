e5443c262718c62435e5821590caf34f4a6f6805
armughanahmed <50084543+armughanahmed@users.noreply.github.com>
2022-12-16 04:34:50 +0500

    Updates

--- common/src/middlewares/error-handler.ts
+++ common/src/middlewares/error-handler.ts
@@ -11,5 +11,6 @@ export const errorHandler = (
     return res.status(err.statusCode).send({ errors: err.seralizeError() });
   }
 
+  console.error(err);
   res.status(400).send({ errors: [{ message: "Something Went Wrong" }] });
 };
