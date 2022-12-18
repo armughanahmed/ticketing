b716d28a547cb28de34aeb342e710295428c840c
armughanahmed <50084543+armughanahmed@users.noreply.github.com>
2022-12-19 00:47:48 +0500

    Updates

--- common/src/events/order-cancelled-event.ts
+++ common/src/events/order-cancelled-event.ts
@@ -5,12 +5,8 @@ export interface OrderCancelledEvent {
   subject: Subjects.OrderCancelled;
   data: {
     id: string;
-    status: OrderStatus;
-    userId: string;
-    expiresAt: string;
     ticket: {
       id: string;
-      price: number;
     };
   };
 }
